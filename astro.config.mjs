4f384b177418511d586cf324004d99a8ab154abb
Pablo Vallejo <pablo@pablovallejo.dev>
2026-09-18 17:32:12 +0200

    feat: correct url and csp applied

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,22 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
+const contentSecurityPolicy = {
+  directives: [
+    "default-src 'self'",
+    "base-uri 'self'",
+    "connect-src 'self'",
+    "font-src 'self'",
+    "form-action 'self'",
+    "frame-ancestors 'none'",
+    "img-src 'self' data:",
+    "manifest-src 'self'",
+    "object-src 'none'",
+  ],
+};
+
 export default defineConfig({
-  site: "https://puente.app",
+  site: "https://puente.pablovallejo.dev",
   output: "static",
   integrations: [
     sitemap({
@@ -16,6 +30,9 @@ export default defineConfig({
       },
     }),
   ],
+  security: {
+    csp: contentSecurityPolicy,
+  },
   i18n: {
     defaultLocale: "es",
     locales: ["es", "ca", "en"],
